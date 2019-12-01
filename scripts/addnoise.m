8482fc5087deee9a55ee2e0fd6366ad3e15dd03e
MichaelAlbert98 <albertclan@frontier.com>
2019-12-01 13:47:17 -0800

    update

--- scripts/addnoise.m
+++ scripts/addnoise.m
@@ -1,7 +1,14 @@
-function noisyim = addnoise(noise, im)
-
+function [orig, noisyim] = AddNoise(noise, image)
+  % noise: Percentage to be used for gaussian noise adding
+  % image: The image to add noise to
+  % noise: Percentage to be used for gaussian noise adding
+  
+  orig=imread(image);
+  orig=im2double(orig);
+  orig = orig(:);
+  
   % Add noise to image
-  v = noise*var(im);
-  noisyim = imnoise(im, 'gaussian', 0, v);
+  v = noise*var(orig);
+  noisyim = imnoise(orig, 'gaussian', 0, v);
   
 endfunction
