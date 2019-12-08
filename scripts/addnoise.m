1a4d967f10cb44f894cc22b20075ce73516f20cb
Michael Albert <albertm4@wwu.edu>
2019-12-07 21:10:09 -0800

    Added custom noise and salt/pepper noise

--- scripts/addnoise.m
+++ scripts/addnoise.m
@@ -1,14 +1,34 @@
-function [orig, noisyim] = AddNoise(noise, image)
+function [orig, noisyim] = addnoise(noise, image, flags)
   % noise: Percentage to be used for gaussian noise adding
   % image: The image to add noise to
   % noise: Percentage to be used for gaussian noise adding
   
   orig=imread(image);
   orig=im2double(orig);
-  orig = orig(:);
   
-  % Add noise to image
-  v = noise*var(orig);
-  noisyim = imnoise(orig, 'gaussian', 0, v);
+  if flags == 0
+    % Add Gaussian noise to image
+    orig = orig(:);
+    v = noise*var(orig);
+    noisyim = imnoise(orig, 'gaussian', 0, v);
+  elseif flags == 1
+    % Add salt and pepper noise to image
+    noisyim = imnoise(orig, 'salt & pepper', noise);
+    orig = orig(:);
+    noisyim = noisyim(:);
+  else
+    % Add noise to image based on distance from center
+    len = length(orig);
+    cent = len/2;
+    for i=1:len
+      for j=1:len
+        dist = sqrt((cent-i)^2+(cent-j)^2);
+        noise = -.5 + rand;
+        noisyim(i,j) = orig(i,j) + (dist/(len))*rand;
+      endfor
+    endfor
+    orig = orig(:);
+    noisyim = noisyim(:);
+  endif
   
 endfunction
